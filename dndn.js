80accbf2cd53baf864b18849d3833e1f811d22f2
saif588 <52879046+saif588@users.noreply.github.com>
2019-07-15 17:56:35 +0200

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,11 @@
+﻿const { Client, RichEmbed } = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+
+
+client.login("توكن البوت الى جبناه بالاخر ")
+
